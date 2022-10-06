cf6ff52a79d034f09e2aec0b67357eea14e75b91
Tobi Balogun <tobi.balo@gmail.com>
2022-10-06 22:33:55 +0100

    complete: 4.4: helper functions and unit tests, step2
    Define a new totalLikes function that receives a list
    of blog posts as a parameter. The function returns the total sum of likes
    in all of the blog posts

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return blogs ? 1 : 0
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => sum += blog.likes
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
