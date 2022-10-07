7f9172e890ea5628303b38503c032f4b49c2ba83
Tobi Balogun <tobi.balo@gmail.com>
2022-10-07 22:17:43 +0100

    4.5*: helper functions and unit tests, step3
    Define a new favoriteBlog function that receives a list of blogs as a parameter.
    The function finds out which blog has most likes.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,19 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const blogsArray = blogs.map(blog => blog.likes)
+  const maxLikes = Math.max.apply(null, blogsArray)
+  const fav = blogs.find(blog => blog.likes === maxLikes)
+  return {
+    title: fav.title,
+    author: fav.author,
+    likes: fav.likes
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog
 }
