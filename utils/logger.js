eb34c5ddf643baee9ae89939d0f88cc7b93bbb2c
Tobi Balogun <tobi.balo@gmail.com>
2022-10-08 11:55:24 +0100

    misc: add functions to check elapsed time

--- utils/logger.js
+++ utils/logger.js
@@ -6,7 +6,13 @@ const error = (...params) => {
   return console.error(...params)
 }
 
+const tick = Date.now()
+
+const log = (v) => console.log(`${v} \n Elapsed: ${Date.now() - tick}ms`)
+
 module.exports = {
   info,
   error,
+  tick,
+  log
 }
