a86e56786917fdbd9bdcc60a2bda3f88e9b650a6
Tobi Balogun <tobi.balo@gmail.com>
2022-10-08 11:58:18 +0100

    4.8: Blog list tests, step1
    Use the supertest package for writing a test that makes an HTTP GET request to the /api/blogs url.
    Verify that the blog list application returns the correct amount of blog posts in the JSON format.

---
+++ tests/bloglist_api.test.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const app = require('../app')
+const supertest = require('supertest')
+const api = supertest(app)
+const Bloglist = require('../models/bloglist')
+const helper = require('./test_helper')
+
+beforeEach(async () => {
+  await Bloglist.deleteMany({})
+
+  const bloglistObject = helper.initialBloglist.map((li) => new Bloglist(li))
+  const bloglistArray = bloglistObject.map((li) => li.save())
+  await Promise.all(bloglistArray)
+})
+
+describe('check environment variables', () => {
+  test('check that node environment is test', () => {
+    expect(process.env.NODE_ENV).toBe('test')
+  })
+})
+
+describe('test the /api/blogs endpoints', () => {
+  const URL = '/api/blogs'
+  test('get request returns the correct amount of blog posts', async () => {
+    const listInDb = await helper.bloglistInDb()
+
+    const response = await api
+      .get(URL)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body.length).toBe(listInDb.length)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
